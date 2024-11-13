ea266275525e10086bf03a832d27e25d7d01f4a2
felipe <felipemoreno148@gmail.com>
2024-11-13 16:40:54 -0300

    adiciona o conteúdo

--- src/SPA/SPA.jsx
+++ src/SPA/SPA.jsx
@@ -3,7 +3,23 @@ import './SPA.css'
 function SPA () {
     return (
         <div> 
-
+            <form>
+            <h1>Olá, bem vindo ao conversor de medidas!</h1>
+            <p>Convertemos de acordo com o <a href="https://abre.ai/ltA5" target='_blank' rel='external'>SI</a> (sistema Internacional de Unidades) </p>
+                <input type="number" placeholder='Digite aqui'/>
+                <select name="Unidades" id="unities">
+                    <option value="km">Kilômetro</option>
+                    <option value="hm">Hectômertro</option>
+                    <option value="dam">Decâmetreo</option>
+                    <option value="m">Metro</option>
+                    <option value="dc">Decímetro</option>
+                    <option value="cm">Centímetro</option>
+                    <option value="mm">Milímetro</option>
+                </select>
+                <p>
+                    Resultado: <p id='res'></p>
+                </p>
+            </form>
         </div>
     )
 }
